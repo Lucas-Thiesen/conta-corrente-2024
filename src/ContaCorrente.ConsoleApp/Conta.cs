fcdf92dd72a86d91fc4bda2b997440a1a0830f0a
Lucas Thiesen <lucasthiesen@LIVE.COM>
2024-04-11 15:44:05 -0300

    90%

---
+++ src/ContaCorrente.ConsoleApp/Conta.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+using System.Threading.Tasks;
+namespace ContaCorrente.ConsoleApp
+{
+    //    Uma conta corrente possui um número, um saldo, um status que informa se ela é especial ou não,
+    //    um limite e um conjunto de movimentações.Uma movimentação possui um valor e uma informação se ela é uma movimentação de crédito ou débito. 
+
+    //Cada conta terá operações de saques, depósitos, visualização de saldo, visualização de extrato e transferência entre contas.
+    //    Cada conta vai ter o nome, o sobrenome e cpf do cliente dono da conta.
+
+    //Uma conta corrente só pode fazer saques desde que o valor não exceda o limite de saque que é o limite + saldo.
+    //    Não precisa implementar a interação com usuário.
+
+
+    public class Conta
+    {
+        public double NumeroConta { get; set; }
+        public decimal LimiteConta { get; set; }
+        public decimal Saldo { get; set; }
+        public DadosUsuario dadosUsuario { get; set; }
+
+        public void Ehespecial()
+        {
+            if (LimiteConta >= 5000)
+                Console.WriteLine("CONTA ESPECIAL");
+            else
+                Console.WriteLine("CONTA PADRÃO");
+        }
+        public decimal MostrarSaldo()
+        {
+            return Saldo;
+        }
+        public void Depositar(decimal ValorDeposito)
+        {
+            Saldo += ValorDeposito;
+        }
+        public void Sacar(decimal ValorSaque)
+        {
+            if (ValorSaque <= Saldo)
+                Saldo -= ValorSaque;
+            else
+                Console.WriteLine("Saldo insuficiente!");
+        }
+        public void Transferencia(decimal ValorTransferencia)
+        {
+            Saldo += ValorTransferencia;
+        }
+
+    }
+}
--- src/ContaCorrente.ConsoleApp/Program.cs
+++ src/ContaCorrente.ConsoleApp/Program.cs
@@ -1,10 +1,68 @@
-﻿namespace ContaCorrente.ConsoleApp
+﻿using System.Security.Cryptography;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ContaCorrente.ConsoleApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Conta conta1 = new Conta();
+            conta1.NumeroConta = 1001;
+            conta1.LimiteConta = 250;
+            conta1.Saldo = 2500;
+
+            conta1.dadosUsuario = new DadosUsuario()
+            {
+                CPF = "10155226898",
+                NomeUsuario = "Lucas Thiesen"
+            };
+
+            Conta conta2 = new Conta();
+            conta2.NumeroConta = 1002;
+            conta2.LimiteConta = 6000;
+            conta2.Saldo = 1000;
+
+            conta2.dadosUsuario = new DadosUsuario()
+            {
+                CPF = "20255226898",
+                NomeUsuario = "Maria Eduarda"
+            };
+
+            Console.WriteLine($"Bem vindo de volta: {conta1.dadosUsuario.NomeUsuario}");
+            Console.WriteLine($"Confirme o seu CPF: {conta1.dadosUsuario.CPF}");
+            Console.ReadKey();
+            Console.WriteLine($"O status da sua conta é: {conta1.Ehespecial}");
+            Console.ReadKey();
+
+            Console.WriteLine($"O saldo atual é de: {conta1.Saldo}");
+            Console.WriteLine("Realizando um depósito de R$500,00");
+            conta1.Depositar(500);
+            Console.WriteLine($"O saldo atual é de:  {conta1.Saldo}");
+            Console.ReadKey();
+            Console.WriteLine("Realizando um saque de 1354");
+            conta1.Sacar(1354);
+            Console.WriteLine($"O saldo atual é de: {conta1.Saldo}");
+            Console.ReadKey();
+            Console.WriteLine("Sacando acima do limite: ");
+            conta1.Sacar(2500);
+            Console.WriteLine($"O saldo atual é de: {conta1.Saldo}");
+            Console.ReadKey();
+            Console.ReadKey();
+            //ver se a conta 2 acima do valor especial retorna que é especial...
+            Console.WriteLine($"Bem vindo de volta: {conta2.dadosUsuario.NomeUsuario}");
+            Console.WriteLine($"Confirme o seu CPF: {conta2.dadosUsuario.CPF}");
+            Console.ReadKey();
+            Console.WriteLine($"O status da sua conta é: {conta2.Ehespecial}");
+            Console.WriteLine($"O saldo atual é de: {conta2.Saldo}");
+            Console.WriteLine("Lucas Thiesen Lhe transferiu R$1000");
+            conta2.Transferencia(1000);
+            Console.WriteLine($"O saldo atual é de: {conta2.Saldo}");
+            Console.ReadKey();
+
+
+
+
         }
     }
 }
